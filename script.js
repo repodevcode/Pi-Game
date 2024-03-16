f4be26cc68efb6ff2eaab4ab899922507b0a4f5c
repodevcode <grdfrlwn-s01@yahoo.com>
2024-03-16 12:47:49 -0400

    attached javascript to html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128"
 
-function userGuessed() {
-
+function userGuessed(el) {
+    console.log(el)
 }
 
 for(let i =0; i<document.getElementsByClassName("guessingButtons").length; i++) {
