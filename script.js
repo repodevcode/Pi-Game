91faa4dfa2c6330693378e10cfd61e0d414608f2
repodevcode <grdfrlwn-s01@yahoo.com>
2024-03-16 12:46:30 -0400

    Added onclick for buttons

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128"
+
+function userGuessed() {
+
+}
+
+for(let i =0; i<document.getElementsByClassName("guessingButtons").length; i++) {
+    document.getElementsByClassName("guessingButtons")[i].addEventListener("click", function () {
+        userGuessed(this)
+    })
+}
