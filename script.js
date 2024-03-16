8058e0684909004edc0b4b380b953ab1a857140a
repodevcode <grdfrlwn-s01@yahoo.com>
2024-03-16 12:58:29 -0400

    updated guessing function

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 let pi = "3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128"
+pi = pi.split("")
+let guessedPi = ""
+let i = 0
 
 function userGuessed(el) {
     console.log(el)
+    if (pi[i] == el.innerHTML) {
+        guessedPi = guessedPi + el.innerHTML
+        document.getElementById("guessedDigitsInput").innerHTML = guessedPi
+        i++
+    }
+    console.log(guessedPi)
 }
 
-for(let i =0; i<document.getElementsByClassName("guessingButtons").length; i++) {
+for (let i = 0; i < document.getElementsByClassName("guessingButtons").length; i++) {
     document.getElementsByClassName("guessingButtons")[i].addEventListener("click", function () {
         userGuessed(this)
     })
