f88a682a6ba8277769002302d6ddc82298c8923a
repodevcode <grdfrlwn-s01@yahoo.com>
2024-03-16 15:45:17 -0400

    fixed issue regarding src

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function userGuessed(el) {
     if (pi[i] == el.innerHTML) {
         guessedPi = guessedPi + el.innerHTML
         document.getElementById("guessedDigitsInput").innerHTML = guessedPi
-        document.getElementById("pi.png").src = "piHappy.png"
+        document.getElementById("pi").src = "piHappy.png"
         i++
     }
     else{
-        document.getElementById("pi.png").src = "piSad.png"
+        document.getElementById("pi").src = "piSad.png"
 
     }
     console.log(guessedPi)
